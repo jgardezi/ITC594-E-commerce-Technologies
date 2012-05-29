13317ad2eedbbe9871d6ae7a67c3c25322da343e
Javed Gardezi <jgardezi@gmail.com>
2012-05-30 03:32:31 +1000

    comment display and post is completed

--- destopversion/layouts/header.php
+++ destopversion/layouts/header.php
@@ -2,6 +2,10 @@
   <head>
     <title>CSU Web Forums</title>
     <link href="stylesheets/main.css" media="all" rel="stylesheet" type="text/css" />
+    
+    <script type="text/javascript" src="../javascripts/jquery.min.js"></script>
+    <script type="text/javascript" src="../javascripts/main.js"></script>
+    
   </head>
   <body>
     <div id="header">
